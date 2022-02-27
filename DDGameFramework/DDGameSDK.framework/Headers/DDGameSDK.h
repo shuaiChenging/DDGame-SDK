f1439fd3cbba9bf4b81b0cbb4ea6145b704abad5
徐志成 <xuzhicheng@jiangduoduo.com>
2022-02-27 21:22:55 +0800

    提交信息

---
+++ DDGameFramework/DDGameSDK.framework/Headers/DDGameSDK.h
@@ -0,0 +1,18 @@
+//
+//  DDGameSDK.h
+//  DDGameSDK
+//
+//  Created by MHJZ on 2022/2/27.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DDGameSDK.
+FOUNDATION_EXPORT double DDGameSDKVersionNumber;
+
+//! Project version string for DDGameSDK.
+FOUNDATION_EXPORT const unsigned char DDGameSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DDGameSDK/PublicHeader.h>
+#import "DDGameManage.h"
+
---
+++ DDGameFramework/DDGameSDK.framework/Headers/RoleInfo.h
@@ -0,0 +1,20 @@
+//
+//  RoleInfo.h
+//  DDGameSDKDemo
+//
+//  Created by MHJZ on 2022/2/23.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RoleInfo : NSObject
+@property (nonatomic, copy) NSString *roleId;
+@property (nonatomic, copy) NSString *roleLevel;
+@property (nonatomic, copy) NSString *roleName;
+@property (nonatomic, copy) NSString *serverId;
+@property (nonatomic, copy) NSString *serverName;
+@end
+
+NS_ASSUME_NONNULL_END
