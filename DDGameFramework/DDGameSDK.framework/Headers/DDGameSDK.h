2f63b3f6e6deeba721e1eb18520af99df0c9ab99
徐志成 <xuzhicheng@jiangduoduo.com>
2022-03-04 11:48:08 +0800

    1.0.2版本

--- DDGameFramework/DDGameSDK.framework/Headers/DDGameSDK.h
+++ DDGameFramework/DDGameSDK.framework/Headers/DDGameSDK.h
@@ -13,6 +13,6 @@ FOUNDATION_EXPORT double DDGameSDKVersionNumber;
 //! Project version string for DDGameSDK.
 FOUNDATION_EXPORT const unsigned char DDGameSDKVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <DDGameSDK/PublicHeader.h>  
+// In this header, you should import all the public headers of your framework using statements like #import <DDGameSDK/PublicHeader.h>
 #import "DDGameManage.h"
 
